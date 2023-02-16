5ffa2e129fd29d4af02cc063aece80c1d8e3618e
Isaque-Morais <isaquemorais5@gmail.com>
2023-02-16 17:09:54 -0300

    Trabalhando com POO

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from pessoa import Pessoa
 
 p_1 = Pessoa('Isaque', 20)
-p_1.comer()
-p_1.falando()
+p_1.comer('maçã')
+p_1.falar('Algum assunto')
+p_1.parar_de_comer()
+p_1.falar('Algum assunto')
+p_1.comer('Peixe')
--- pessoa.py
+++ pessoa.py
@@ -5,10 +5,42 @@ class Pessoa:
         self.comendo = comendo
         self.falando = falando
 
-    def comer(self):
-        print(f'{self.nome} que tem {self.idade} de idade, está comendo.')
+    def falar(self, assunto):
+        if self.comendo:
+            print(f'{self.comendo} não pode falar comendo.')
+            return
+        
+        if self.falando:
+            print(f'{self.nome} já está falando.')
+            return
+        
+        print(f'{self.nome} está falando sobre {assunto}')
+        self.falando = True
+
+    def parar_de_falar(self):
+        if not self.falando:
+            print(f'{self.nome} não está falando')        
+            return
+
+        print(f'{self.nome} parou de falar')
+        self.comendo = False
+
+    def comer(self, alimento):
+        if self.comendo:
+            print(f'{self.nome} já está comendo.')
+            return
+        
+        if self.falando:
+            print(f'{self.nome} não pode comer falando.')
+            return
+
+        print(f'{self.nome} está comendo {alimento}.')
         self.comendo = True
 
-    def falando(self):
-        print(f'O {self.nome} está {self.falando}!!!')
-        self.falando = True
+    def parar_de_comer(self):
+        if not self.comendo:
+            print(f'{self.nome} não está comendo.')
+            return
+
+        print(f'{self.nome} parou de comer')
+        self.comendo = False
