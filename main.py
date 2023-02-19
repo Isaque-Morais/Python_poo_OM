0d47d5f4cac71c06c1e46a13ccdd8f82fab14527
Isaque-Morais <isaquemorais5@gmail.com>
2023-02-19 11:17:51 -0300

    chore: update master

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from pessoa import Pessoa
 
 p_1 = Pessoa('Isaque', 20)
 p_2 = Pessoa('Ana', 21)
+p_3 = Pessoa('Fernando', 30)
 
 print(p_1.get_month_nascimento())
 print(p_2.get_month_nascimento())
+print(p_3.get_month_nascimento())
